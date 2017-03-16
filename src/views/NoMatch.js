bbc906d4e806f8b81cb25aea4be69f761f64a379
Benjamin Singleton <benfsingleton@gmail.com>
2017-03-15 23:30:41 -0400

    404 page updated with material-ui

--- src/views/NoMatch.js
+++ src/views/NoMatch.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import {Card, CardTitle, CardText} from 'material-ui/Card';
 
 import Layout from '../components/Layout'
 
 const NoMatch = () => {
     return (
 		<Layout>
-			<div className="middle-box text-center animated fadeInDown">
-				<h1>404</h1>
-				<h3 className="font-bold">Page Not Found</h3>
-				<div className="error-desc">
-					Sorry, but the page you are looking for has note been found. 
-					Try checking the URL for error, then hit the refresh button on your browser 
-					or try found something else in our app.
+			<div className="row" style={{margin: '100px 2px 30px 2px'}}>
+				<div className="col-xs-12 col-sm-offset-3 col-sm-6 col-md-offset-4 col-md-4 col-lg-offset-4 col-lg-4">
+                    <Card>
+						<CardTitle title="404" subtitle="Page Not Found" />
+						<CardText>
+							<div>Sorry, but the page you are looking for has note been found. 
+								Try checking the URL for error, then hit the refresh button on your browser 
+								or try found something else in our app.
+							</div>
+						</CardText>
+					</Card>
 				</div>
 			</div>
 		</Layout>
