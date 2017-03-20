6c85770afca601048f6cbc37ccf07df825e32448
Benjamin Singleton <benfsingleton@gmail.com>
2017-03-20 17:00:38 -0400

    Added menu to navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,16 +1,42 @@
-import React from 'react'
+import React, { Component } from 'react'
 import AppBar from 'material-ui/AppBar';
 import FlatButton from 'material-ui/FlatButton';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
 
-const Navigation = (props) => {
+class Navigation extends Component {
+    constructor() {
+        super()
+        this.state = {open: false}
+        this.handleToggle = this.handleToggle.bind(this)
+    }
 
-    return (
-        <AppBar 
-            title="Gotham Police Crime Tips"
-            iconElementRight={props.uid ? <FlatButton label="Log Out" href="logout" /> : null} 
-            zDepth={0}
-        />
-    );
+    handleToggle = () => this.setState({open: !this.state.open});
+
+    closeDrawer = () => this.setState({open: false});
+
+    render() {
+        return (
+            <div>
+                <AppBar 
+                    title="Gotham Police Crime Tips"
+                    iconElementRight={this.props.uid ? <FlatButton label="Log Out" href="logout" /> : null} 
+                    onLeftIconButtonTouchTap={this.handleToggle}
+                    zDepth={0}
+                />
+                <Drawer open={this.state.open} onRequestChange={this.closeDrawer} docked={false}>
+                    {this.props.uid ? 
+                        <MenuItem>Settings</MenuItem> 
+                    :
+                        <div>
+                            <MenuItem>FAQs</MenuItem>
+                            <MenuItem>About</MenuItem>
+                        </div>
+                    }
+                </Drawer>
+            </div>
+        )
+    }
 }
 
 export default Navigation
