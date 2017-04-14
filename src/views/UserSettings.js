555b66f481d15b145c99fdbe12fe51991d167c0f
Benjamin Singleton <benfsingleton@gmail.com>
2017-04-14 00:04:04 -0400

    [WIP] change password

--- src/views/UserSettings.js
+++ src/views/UserSettings.js
@@ -6,8 +6,88 @@ import Toggle from 'material-ui/Toggle';
 import Divider from 'material-ui/Divider';
 
 import Layout from '../components/Layout'
+import {databaseRef, firebaseAuth} from '../helpers/constants'
 
-class UserSettings extends Component {
+export default class UserSettings extends Component {
+  constructor () {
+    super()
+    this.state = {
+      rank: null,
+      firstName: null,
+      lastName: null,
+      notifications: true,
+      password: '',
+      passwordRepeat: '',
+      errorText: null,
+      passwordMsg: ''
+    }
+    this.onToggle = this.onToggle.bind(this)
+    this.handleTextChange = this.handleTextChange.bind(this)
+    this.changePassword = this.changePassword.bind(this)
+  }
+
+  componentWillMount() {
+    databaseRef.child(`users/${this.props.uid}/account`).once('value', function(snapshot) {
+      const userDetails = snapshot.val()
+      this.setState({
+        rank: userDetails.rank,
+        firstName: userDetails.firstName,
+        lastName: userDetails.lastName,
+        notifications: userDetails.notifications
+      })
+    }.bind(this));
+
+    databaseRef.child(`users/${this.props.uid}/activity`).limitToLast(5).once('value', function(snapshot) {
+      const userActivity = snapshot.val()
+      this.setState({userActivity})
+    }.bind(this));
+
+  }
+
+  handleTextChange(name, event) {
+    this.setState({[name]: event.target.value})
+  }
+
+  onToggle() {
+    const notifications = !this.state.notifications
+    databaseRef.child(`users/${this.props.uid}/account`).update({notifications})
+    this.setState({notifications})
+  }
+
+  changePassword() {
+    const uid = firebaseAuth().currentUser.uid
+    const newPassword = this.state.password
+    const newPasswordRepeate = this.state.passwordRepeat
+
+    if (newPassword === newPasswordRepeate) {
+      firebaseAuth().updateUser(uid, {
+        password: newPassword,
+      })
+        .then(function(userRecord) {
+          console.log('success');
+          this.setState({
+            passwordMsg: "Your password has been updated.",
+            password: '',
+            passwordRepeat: ''
+          });
+        })
+        .catch(function(error) {
+          console.log("Error updating user:", error);
+          this.setState({
+            passwordMsg: "An error has occurred. Please try again.",
+            password: '',
+            passwordRepeat: ''
+          });
+        });
+    } else {
+      this.setState({
+        password: '',
+        passwordRepeat: '',
+        errorText: "The passwords do not match.",
+      });
+    }
+  }
+
   render() {
     return (
       <Layout uid={this.props.uid} logout={this.props.logout}>
@@ -17,19 +97,39 @@ class UserSettings extends Component {
               <CardHeader title="My Settings"/>
               <Divider />
               <CardText>
-                <h2>Rank Firstname Lastname</h2>
+                <h2>{`${this.state.rank} ${this.state.firstName} ${this.state.lastName}`}</h2>
                 <br />
-                <Divider />
-                <h3>Change Password</h3>
-                <p>Type in a new password (twice) to change it</p>
-                <TextField hintText="Password" />
-                <TextField hintText="Password again" style={{marginBottom: '20px'}} />
-                <RaisedButton label="Change Password" primary={true} style={{marginBottom: '20px'}}/>
-                <Divider />
                 <h3>Notifications</h3>
                 <Toggle
                   label="Receive emails when new tips are submitted"
-                  defaultToggled={true}
+                  toggled={this.state.notifications}
+                  style={{marginBottom: '20px'}}
+                  onToggle={this.onToggle}
+                />
+                <Divider />
+                <h3>Change Password</h3>
+                <p>Type in a new password (twice) to change it</p>
+                {this.state.passwordMsg && <p>{this.state.passwordMsg}</p>}
+                <TextField
+                  hintText="New password" 
+                  type="password"
+                  value={this.state.password}
+                  errorText={this.state.errorText}
+                  onChange={this.handleTextChange.bind(null, "password")}
+                />
+                <TextField 
+                  hintText="Repeat new password"
+                  type="password"
+                  value={this.state.passwordRepeat}
+                  errorText={this.state.errorText}
+                  style={{marginBottom: '20px'}}
+                  onChange={this.handleTextChange.bind(null, "passwordRepeat")}
+                />
+                <RaisedButton 
+                  label="Change Password" 
+                  primary={true} 
+                  style={{marginBottom: '10px'}}
+                  onTouchTap={() => this.changePassword()}
                 />
               </CardText>
             </Card>
@@ -61,5 +161,3 @@ class UserSettings extends Component {
     );
   }
 }
-
-export default UserSettings;
