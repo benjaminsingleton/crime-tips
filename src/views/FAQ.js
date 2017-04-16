0eab76f80260ee8014fb26e16c8bc7a07268b3a0
Benjamin Singleton <benfsingleton@gmail.com>
2017-04-15 23:45:44 -0400

    Updated language

--- src/views/FAQ.js
+++ src/views/FAQ.js
@@ -17,28 +17,51 @@ const FAQ = () => {
 									<b>What is Crime Tips 24/7?</b>
 								</p>
 								<p>
-									Crime Tips 24/7 is a website that allows police departments to receive anonymous tips from concerned members of the public.
+									Crime Tips 24/7 is a website that allows police departments to receive anonymous tips from 
+                  concerned members of the public.
+								</p>
+								<br />
+                <p className="prompt">
+									<b>What is a tip?</b>
+								</p>
+								<p>
+									A tip is information about unsolved crimes, wanted fugitives, narcotics activity, bullying,
+                  and missing persons, among other issues of interest to the police. Any information you can 
+                  provide might be of value, no matter how important or unimportant you believe it to be. Tipsters 
+                  are not snitches -- they care about their community.
+								</p>
+								<br />
+                <p className="prompt">
+									<b>What is not a tip?</b>
+								</p>
+								<p>
+									The Crime Stoppers program is not a substitute for the 9-1-1 emergency system. In the event 
+                  of an emergency, tipsters are encouraged to call the 9-1-1 emergency system immediately!
 								</p>
 								<br />
 								<p className="prompt">
 									<b>What will you do with the information you get from a tip?</b>
 								</p>
 								<p>
-									Once a tip is received, a detective will review the information and will determine who should investigate the information provided.
+									Once a tip is received, a detective will review the information and will determine who should 
+                  investigate the information provided.
 								</p>
 								<br />
 								<p className="prompt">
 									<b>How do I know that I will remain anonymous?</b>
 								</p>
 								<p>
-									We guarantee complete anonymity — no IP addresses or any other data (other than the tip). In fact, we are so transparent about this issue that our source code is publicly available on Github.
+									We guarantee complete anonymity — no IP addresses or any other data (other than the tip). In 
+                  fact, we are so transparent about this issue that our source code is publicly available on Github.
 								</p>
 								<br />
 								<p className="prompt">
 									<b>I want to provide information, but I’m concerned. What should I do?</b>
 								</p>
 								<p>
-									We understand that providing information can put you in an uncomfortable situation, but the community depends on people like you to identify criminals so they won’t victimize more people in the future. As always, call 911 if you are in imminent danger.
+									We understand that providing information can put you in an uncomfortable situation, but the 
+                  community depends on people like you to identify criminals so they won’t victimize more people 
+                  in the future. As always, call 911 if you are in imminent danger.
 								</p>
 								<br />
 								<p className="prompt">
@@ -49,10 +72,22 @@ const FAQ = () => {
 								</p>
 								<br />
 								<p className="prompt">
-									<b>How do I claim my reward?</b>
+									<b>How do I know if I am eligible to receive a reward?</b>
 								</p>
 								<p>
-									[ Department answer ] Upon successfully submitting a tip online, a confirmation code will be displayed on the screen. You may call the Department at any time to check on the status of your tip and whether a reward will be awarded.
+									Contact the Department to update your tip information or to check status on your reward. 
+                  Please have your tip number ready as no information will be provided to you without it. 
+                  A representative of the Department will let you know if an arrest has been made and a reward 
+                  is available or the status of the investigation.
+								</p>
+                <p className="prompt">
+									<b>How do I collect my reward?</b>
+								</p>
+								<p>
+									After you check the status of your tip and verified that you are eligible to receive a reward, 
+                  a representative from the Department will provide you with instructions for collecting your 
+                  reward from the bank. You will not need to provide personal identification to the bank; you will 
+                  be receiving cash.
 								</p>
 							</CardText>
 						</Card>
