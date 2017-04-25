d1f78c10dd7b1cacd203e456a7758ef3c3172988
Benjamin Singleton <bensingleton@SWL-NY-C1MRTNFSH3QF.local>
2017-04-25 11:25:17 -0400

    Change to language variables

--- src/components/TipFormContainer.js
+++ src/components/TipFormContainer.js
@@ -4,94 +4,66 @@ import { Card, Button, Container, Breadcrumb, Icon } from 'semantic-ui-react'
 
 const TipFormContainer = (props) => {
   return (
-    <Card centered fluid>
-      <Card.Content header={props.title} meta='All tips are completely anonymous. Your community needs your help.' />
-      {/*<Card.Content>
-        <div className="ui one column stackable grid container">
-          <Step.Group size='mini'>
-            <Step active icon='marker' title='Incident' />
-            <Step icon='user' title='Suspect' />
-            <Step disabled icon='car' title='Vehicle' description='optional' />
-            <Step disabled icon='medkit' title='Drugs' description='optional' />
-            <Step disabled icon='cloud upload' title='Media' description='optional' />
-            <Step icon='check' title='Finish' />
-          </Step.Group>
-        </div>
-        <table className="ui unstackable table">
-          <tbody>
-            <tr>
-              <td>Incident</td>
-              <td>Suspect</td>
-              <td>Vehicle</td>
-              <td>Drugs</td>
-              <td>Media</td>
-              <td>Finish</td>
-            </tr>
-          </tbody>
-        </table>
-      </Card.Content>*/}
+    <Card centered fluid style={{marginBottom: '14px'}}>
+      <Card.Content header={props.title} meta={props.lang.cardSubtitle} />
       <Card.Content>
         <Container textAlign='center'>
           <Breadcrumb>
             <Breadcrumb.Section active={props.activeModule === 'intro'}>
-              <Icon disabled={props.activeModule !== 'intro'} name='marker' />Incident
+              <Icon disabled={props.activeModule !== 'intro'} name='marker' />{props.lang.incident}
             </Breadcrumb.Section>
             <Breadcrumb.Divider icon='right arrow' />
             {props.showFormModule.suspect === true && 
               <Breadcrumb.Section active={props.activeModule === 'suspect'}>
-                <Icon disabled={props.activeModule !== 'suspect'} name='user' />Suspect
+                <Icon disabled={props.activeModule !== 'suspect'} name='user' />{props.lang.suspect}
               </Breadcrumb.Section>
             }
             {props.showFormModule.suspect === true && <Breadcrumb.Divider icon='right arrow' />}
             {props.showFormModule.vehicle === true && 
               <Breadcrumb.Section active={props.activeModule === 'vehicle'}>
-                <Icon disabled={props.activeModule !== 'vehicle'} name='car' />Vehicle
+                <Icon disabled={props.activeModule !== 'vehicle'} name='car' />{props.lang.vehicle}
               </Breadcrumb.Section>
             }
             {props.showFormModule.vehicle === true && <Breadcrumb.Divider icon='right arrow' />}
             {props.showFormModule.drugs === true && 
               <Breadcrumb.Section active={props.activeModule === 'drugs'}>
-                <Icon disabled={props.activeModule !== 'drugs'} name='medkit' />Drugs
+                <Icon disabled={props.activeModule !== 'drugs'} name='medkit' />{props.lang.drugs}
               </Breadcrumb.Section>
             }
             {props.showFormModule.drugs === true && <Breadcrumb.Divider icon='right arrow' />}
             {props.showFormModule.media === true && 
               <Breadcrumb.Section active={props.activeModule === 'media'}>
-                <Icon disabled={props.activeModule !== 'media'} name='cloud upload' />Media
+                <Icon disabled={props.activeModule !== 'media'} name='cloud upload' />{props.lang.mediaBreadcrumb}
               </Breadcrumb.Section>
             }
             {props.showFormModule.media === true && <Breadcrumb.Divider icon='right arrow' />}
             <Breadcrumb.Section active={props.activeModule === 'final'}>
-              <Icon disabled={props.activeModule !== 'final'} name='check' />Submit
+              <Icon disabled={props.activeModule !== 'final'} name='check' />{props.lang.final}
             </Breadcrumb.Section>
           </Breadcrumb>
         </Container>
       </Card.Content>
       <Card.Content>
-        {props.OptionalMsg &&
-          <p>
-            All of our questions are <b>optional</b> so feel free to skip any questions you can’t answer.
-          </p>
-        }
+        {props.OptionalMsg && props.lang.OptionalMsg}
         {props.children}
       </Card.Content>
       <Card.Content>
         {props.previousButton &&
           <Button
-            content="Previous"
+            content={props.lang.previous}
             onClick={() => props.changeFormWizardIndex('previous')}
           />
         }
         {props.nextButton &&
           <Button
-            content="Next"
+            content={props.lang.next}
             color='violet'
             onClick={() => props.changeFormWizardIndex('next')}
           />
         }
         {props.showSubmit &&
           <Button 
-            content="Submit" 
+            content={props.lang.submit}
             color='violet'
             onClick={(e) => props.createTip(e)} 
           />
