b12b9d957eaf9120a4a6d351b976f34d38cc749c
Benjamin Singleton <benfsingleton@gmail.com>
2017-05-03 08:23:38 -0400

    Filtering tips now works

--- src/components/DashboardLeftPanel.js
+++ src/components/DashboardLeftPanel.js
@@ -4,35 +4,55 @@ import { Card, Button, Icon, Menu, Label } from 'semantic-ui-react'
 export default class DashboardLeftPanel extends Component {
   state = { activeItem: 'inbox' }
 
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+  handleItemClick = (e, { name }) => {
+    switch (name) {
+      case 'inbox':
+        this.props.filterTips('archived', false)
+        break;
+      case 'abandoned':
+        this.props.filterTips('abandoned', true)
+        break;
+      case 'important':
+        this.props.filterTips('important', true)
+        break;
+      case 'archive':
+        this.props.filterTips('archived', true)
+        break;
+      case 'draft':
+        this.props.filterTips('draft', true)
+        break;
+      default:
+        console.error('invalid filterTip name')
+    }
+    this.setState({ activeItem: name })
+  }
 
   render() {
     const { activeItem } = this.state
-    const { filterTips, changePanel } = this.props
     return (
       <Card fluid>
         <Card.Content>
-          <Button content='New Tip' fluid onClick={() => changePanel('form')} />
+          <Button content='New Tip' fluid onClick={() => this.props.changePanel('form')} />
           <h4>FOLDERS</h4>
           <Menu vertical fluid>
-            <Menu.Item name='inbox' active={activeItem === 'inbox'} onClick={() => {this.handleItemClick(); filterTips('archived', false)}}>
-              <Label color='teal'>1</Label>
+            <Menu.Item name='inbox' active={activeItem === 'inbox'} onClick={this.handleItemClick}>
+              <Label color='teal'>#</Label>
                <Icon name='inbox' /> Tip Inbox
             </Menu.Item>
-            <Menu.Item name='abandoned' active={activeItem === 'abandoned'} onClick={() => {this.handleItemClick(); filterTips('', false)}}>
-              <Label>51</Label>
+            <Menu.Item name='abandoned' active={activeItem === 'abandoned'} onClick={this.handleItemClick}>
+              <Label>#</Label>
               <Icon name='feed' /> Abandoned Tips
             </Menu.Item>
-            <Menu.Item name='important' active={activeItem === 'important'} onClick={() => {this.handleItemClick(); filterTips('important', true)}}>
-              <Label>1</Label>
+            <Menu.Item name='important' active={activeItem === 'important'} onClick={this.handleItemClick}>
+              <Label>#</Label>
               <Icon name='star' /> Important
             </Menu.Item>
-            <Menu.Item name='archive' active={activeItem === 'archive'} onClick={() => {this.handleItemClick(); filterTips('archived', true)}}>
-              <Label>1</Label>
+            <Menu.Item name='archive' active={activeItem === 'archive'} onClick={this.handleItemClick}>
+              <Label>#</Label>
               <Icon name='archive' /> Archive
             </Menu.Item>
-            <Menu.Item name='draft' active={activeItem === 'draft'} onClick={() => {this.handleItemClick(); filterTips('', false)}}>
-              <Label>1</Label>
+            <Menu.Item name='draft' active={activeItem === 'draft'} onClick={this.handleItemClick}>
+              <Label>#</Label>
               <Icon name='mail square' /> Tip Drafts
             </Menu.Item>
           </Menu>
