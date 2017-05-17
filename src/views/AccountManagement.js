2c9d4f4ca36d23866127e80c852eb269c403d7c1
Benjamin Singleton <bensingleton@SWL-NY-C1MRTNFSH3QF.local>
2017-05-17 15:41:47 -0400

    neha

--- src/views/AccountManagement.js
+++ src/views/AccountManagement.js
@@ -3,8 +3,6 @@ import { Grid, Card, Segment, Form, Button, Table, Message } from 'semantic-ui-r
 import Layout from '../components/Layout'
 import { firebaseApp } from '../helpers/firebase'
 
-// neha comment
-
 class CreateUser extends Component {
   state = {
     success: false,
