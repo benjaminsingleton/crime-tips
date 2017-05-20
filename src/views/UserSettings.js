b37c36c1cd735fab36c920df7157b7a6d16f47f7
Benjamin Singleton <benfsingleton@gmail.com>
2017-05-19 22:46:27 -0400

    Linting + all neha changes except combining password and email forms

--- src/views/UserSettings.js
+++ src/views/UserSettings.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
-import { Grid, Card, Form, Radio, Divider, Message } from 'semantic-ui-react'
-import Layout from '../components/Layout'
-import { firebaseApp } from '../helpers/firebase'
-import { relativeTime, reverse } from '../helpers/helpers'
-
-// neha
+import { Link } from 'react-router-dom';
+import { Grid, Card, Form, Radio, Divider, Message } from 'semantic-ui-react';
+import Layout from '../components/Layout';
+import { firebaseApp } from '../helpers/firebase';
+import { relativeTime, reverse } from '../helpers/helpers';
 
 export default class UserSettings extends Component {
   state = {
@@ -17,152 +15,147 @@ export default class UserSettings extends Component {
     passwordMsg: '',
     newEmail: '',
     newEmailRepeat: '',
-    emailMsg: ''
+    emailMsg: '',
   }
 
   componentWillMount = () => {
     firebaseApp.database().ref(`users/${this.state.uid}`).on('value', (snapshot) => {
-      const userDetails = snapshot.val()
-      this.setState({ userDetails })
+      this.setState({ userDetails: snapshot.val() });
     });
 
-    firebaseApp.database().ref(`userActivity/${this.state.uid}`).limitToLast(5).on('value', (snapshot) => {
-      const userActivity = snapshot.val()
-      this.setState({ userActivity })
-    });
+    firebaseApp.database().ref(`userActivity/${this.state.uid}`).limitToLast(5)
+      .on('value', snapshot => this.setState({ userActivity: snapshot.val() }));
   }
 
   componentWillUnmount = () => {
-    firebaseApp.database().ref(`users/${this.state.uid}`).off()
-    firebaseApp.database().ref(`userActivity/${this.state.uid}`).off()
+    firebaseApp.database().ref(`users/${this.state.uid}`).off();
+    firebaseApp.database().ref(`userActivity/${this.state.uid}`).off();
   }
-
-  handleInputChange = (e, { name, value }) => this.setState({[name]: value})
 
   onToggle = () => {
-    const notifications = !this.state.userDetails.notifications
-    firebaseApp.database().ref(`users/${this.state.uid}`).update({ notifications })
-    this.setState({notifications})
+    const notifications = !this.state.userDetails.notifications;
+    firebaseApp.database().ref(`users/${this.state.uid}`).update({ notifications });
+    this.setState({ notifications });
   }
 
+  handleInputChange = (e, { name, value }) => this.setState({ [name]: value });
+
   changePassword = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     const user = firebaseApp.auth().currentUser;
-    const newPassword = this.state.newPassword
-    const newPasswordRepeat = this.state.newPasswordRepeat
-    const timestamp = Date.now()
+    const newPassword = this.state.newPassword;
+    const newPasswordRepeat = this.state.newPasswordRepeat;
+    const timestamp = Date.now();
 
     if (newPassword === newPasswordRepeat) {
       user.updatePassword(newPassword).then(() => {
         firebaseApp.database().ref(`userActivity/${this.state.uid}`).push({
           action: 'changePassword',
-          timestamp: timestamp
+          timestamp,
         });
         this.setState({
           passwordMsgStatus: 'success',
-          passwordMsg: "Your password has been updated.",
+          passwordMsg: 'Your password has been updated.',
           newPassword: '',
-          newPasswordRepeat: ''
+          newPasswordRepeat: '',
         });
       }, (error) => {
         this.setState({
           passwordMsgStatus: 'error',
           passwordMsg: error.message,
           newPassword: '',
-          newPasswordRepeat: ''
+          newPasswordRepeat: '',
         });
       });
     } else {
       this.setState({
         passwordMsgStatus: 'error',
-        passwordMsg: "The passwords do not match. Try again.",
+        passwordMsg: 'The passwords do not match. Try again.',
         newPassword: '',
-        newPasswordRepeat: ''
+        newPasswordRepeat: '',
       });
     }
   }
 
   changeEmail = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     const user = firebaseApp.auth().currentUser;
-    const newEmail = this.state.newEmail
-    const newEmailRepeat = this.state.newEmailRepeat
-    const timestamp = Date.now()
+    const newEmail = this.state.newEmail;
+    const newEmailRepeat = this.state.newEmailRepeat;
+    const timestamp = Date.now();
 
     if (newEmail === newEmailRepeat) {
       user.updateEmail(newEmail).then(() => {
         firebaseApp.database().ref(`userActivity/${this.state.uid}/`).push({
           action: 'changeEmail',
-          timestamp: timestamp
+          timestamp,
         });
         this.setState({
           emailMsgStatus: 'success',
-          emailMsg: "Your email has been updated.",
+          emailMsg: 'Your email has been updated.',
           newEmail: '',
-          newEmailRepeat: ''
+          newEmailRepeat: '',
         });
       }, (error) => {
         this.setState({
           emailMsgStatus: 'error',
           emailMsg: error.message,
           newEmail: '',
-          newEmailRepeat: ''
+          newEmailRepeat: '',
         });
       });
     } else {
       this.setState({
         emailMsgStatus: 'error',
-        emailMsg: "The emails do not match. Try again.",
+        emailMsg: 'The emails do not match. Try again.',
         newEmail: '',
-        newEmailRepeat: ''
+        newEmailRepeat: '',
       });
     }
   }
 
   render() {
-    const { userActivity, userDetails } = this.state
-    const email = firebaseApp.auth().currentUser.email
+    const { userActivity, userDetails } = this.state;
+    const email = firebaseApp.auth().currentUser.email;
 
     function activityText(key) {
-      const tipLink = <Link to={`tip/${key}`} style={{textDecoration: 'none'}}>tip</Link>
+      const tipLink = <Link to={`tip/${key}`} style={{ textDecoration: 'none' }}>tip</Link>;
 
       switch (userActivity[key].action) {
         case 'read':
-          return <span>read a {tipLink}.</span>
+          return <span>read a {tipLink}.</span>;
         case 'important':
           if (userActivity[key].status === true) {
-            return <span>flagged a {tipLink} as important.</span>
-          } else {
-            return <span>unflagged a {tipLink} as important.</span>
+            return <span>flagged a {tipLink} as important.</span>;
           }
+          return <span>unflagged a {tipLink} as important.</span>;
         case 'archived':
           if (userActivity[key].status === true) {
-              return <span>archived a {tipLink}.</span>
-            } else {
-              return <span>unarchived a {tipLink}.</span>
-            }
+            return <span>archived a {tipLink}.</span>;
+          }
+          return <span>unarchived a {tipLink}.</span>;
         case 'changePassword':
-          return 'changed their password.'
+          return 'changed their password.';
         case 'changeEmail':
-          return 'changed their email.'
+          return 'changed their email.';
         default:
-          console.error("Invalid user activity type.")
+          return null;
       }
     }
 
-    let userActivityToDisplay = null
+    let userActivityToDisplay = null;
     if (userActivity) {
-      const userActivityReversed = reverse(userActivity)
-      userActivityToDisplay = Object.keys(userActivityReversed).map(key =>  
-                                  <div key={key}>
-                                    <p style={{color: 'rgb(117, 117, 117)'}}>{relativeTime(userActivity[key].timestamp)}</p>
-                                    <p><b>{`${userDetails.firstName} ${userDetails.lastName}`}</b> {activityText(key)}</p>
-                                    <Divider />
-                                  </div>)
+      const userActivityReversed = reverse(userActivity);
+      userActivityToDisplay = Object.keys(userActivityReversed).map(key =>
+        (<div key={key}>
+          <p style={{ color: 'rgb(117, 117, 117)' }}>{relativeTime(userActivity[key].timestamp)}</p>
+          <p><b>{`${userDetails.firstName} ${userDetails.lastName}`}</b> {activityText(key)}</p>
+          <Divider />
+        </div>));
     }
-    
+
     return (
-      <Layout uid={this.state.uid} logout={this.props.logout}>
+      <Layout>
         <Grid columns={3} centered>
           <Grid.Row>
             <Grid.Column mobile={16} tablet={5} computer={5} largeScreen={5}>
@@ -171,94 +164,96 @@ export default class UserSettings extends Component {
                 <Card.Content>
                   <h2>{`${userDetails.rank} ${userDetails.firstName} ${userDetails.lastName}`}</h2>
                   <h3>Notifications</h3>
-                  <Radio 
+                  <Radio
                     toggle
-                    label='Receive emails when new tips are submitted'
+                    label="Receive emails when new tips are submitted"
                     checked={userDetails.notifications}
-                    style={{marginBottom: '20px'}}
+                    style={{ marginBottom: '20px' }}
                     onChange={this.onToggle}
                   />
                   <Divider />
                   <h3>Change Email</h3>
-                  <Form 
+                  <Form
                     error={this.state.emailMsgStatus === 'error'}
                     success={this.state.emailMsgStatus === 'success'}
-                    onSubmit={(e) => this.changeEmail(e)}>
-                  <p>Current email: {email}</p>
-                  <p>Type in a new email (twice) to change it</p>
-                  <Message
-                    success
-                    content={this.state.emailMsg}
-                  />
-                  <Message
-                    error
-                    content={this.state.emailMsg}
-                  />
-                  <Form.Input
-                    placeholder='New email'
-                    type='email'
-                    name='newEmail'
-                    value={this.state.newEmail}
-                    onChange={this.handleInputChange}
-                  />
-                  <Form.Input 
-                    placeholder='Repeat new email'
-                    type='email'
-                    name='newEmailRepeat'
-                    value={this.state.newEmailRepeat}
-                    onChange={this.handleInputChange}
-                  />
-                  <Form.Button 
-                    content='Change Email'
-                    color='violet' 
-                  />
+                    onSubmit={e => this.changeEmail(e)}
+                  >
+                    <p>Current email: {email}</p>
+                    <p>Type in a new email (twice) to change it</p>
+                    <Message
+                      success
+                      content={this.state.emailMsg}
+                    />
+                    <Message
+                      error
+                      content={this.state.emailMsg}
+                    />
+                    <Form.Input
+                      placeholder="New email"
+                      type="email"
+                      name="newEmail"
+                      value={this.state.newEmail}
+                      onChange={this.handleInputChange}
+                    />
+                    <Form.Input
+                      placeholder="Repeat new email"
+                      type="email"
+                      name="newEmailRepeat"
+                      value={this.state.newEmailRepeat}
+                      onChange={this.handleInputChange}
+                    />
+                    <Form.Button
+                      content="Change Email"
+                      color="violet"
+                    />
                   </Form>
                 </Card.Content>
               </Card>
             </Grid.Column>
             <Grid.Column mobile={16} tablet={5} computer={5} largeScreen={5}>
               <Card fluid>
-                <Card.Content header='Change Password' />
+                <Card.Content header="Change Password" />
                 <Card.Content>
                   <h3>Change Password</h3>
-                  <Form 
-                      error={this.state.passwordMsgStatus === 'error'}
-                      success={this.state.passwordMsgStatus === 'success'}
-                      onSubmit={(e) => this.changePassword(e)}>
-                  <p>Type in a new password (twice) to change it</p>
-                  <Message
-                    error
-                    content={this.state.passwordMsg}
-                  />
-                  <Message
-                    success
-                    content={this.state.passwordMsg}
-                  />
-                  <Form.Input
-                    placeholder='New password'
-                    type='password'
-                    name='newPassword'
-                    value={this.state.newPassword}
-                    onChange={this.handleInputChange}
-                  />
-                  <Form.Input 
-                    placeholder='Repeat new password'
-                    type='password'
-                    name='newPasswordRepeat'
-                    value={this.state.newPasswordRepeat}
-                    onChange={this.handleInputChange}
-                  />
-                  <Form.Button 
-                    content='Change Password'
-                    color='violet' 
-                  />
+                  <Form
+                    error={this.state.passwordMsgStatus === 'error'}
+                    success={this.state.passwordMsgStatus === 'success'}
+                    onSubmit={e => this.changePassword(e)}
+                  >
+                    <p>Type in a new password (twice) to change it</p>
+                    <Message
+                      error
+                      content={this.state.passwordMsg}
+                    />
+                    <Message
+                      success
+                      content={this.state.passwordMsg}
+                    />
+                    <Form.Input
+                      placeholder="New password"
+                      type="password"
+                      name="newPassword"
+                      value={this.state.newPassword}
+                      onChange={this.handleInputChange}
+                    />
+                    <Form.Input
+                      placeholder="Repeat new password"
+                      type="password"
+                      name="newPasswordRepeat"
+                      value={this.state.newPasswordRepeat}
+                      onChange={this.handleInputChange}
+                    />
+                    <Form.Button
+                      content="Change Password"
+                      color="violet"
+                    />
                   </Form>
                 </Card.Content>
               </Card>
             </Grid.Column>
             <Grid.Column mobile={16} tablet={5} computer={5} largeScreen={5}>
               <Card fluid>
-                <Card.Content header='Recent Activity' />
+                <Card.Content header="Recent Activity" />
                 <Card.Content>
                   {userActivityToDisplay}
                 </Card.Content>
